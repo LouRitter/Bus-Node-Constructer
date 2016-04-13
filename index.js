1cbed8ad7b2618e7619d0b0ed63877dacc01cd69
LouRitter <louritter1@gmail.com>
2016-04-13 18:38:44 -0400

    slightly less broken hw

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var Students = require('./student.js');
 var bus= require('./bus.js');
 
 prompt.start();
-function makeBus(){
 var Busses= new bus('Joe Carlo','Yellow',20);
-Busses.studentEntersBus(pupil);
-Busses.busChatter();
-};
+
 function makeStudent(){
 prompt.get(['name','gender','grade','GPA','detentions','sleepingInClass','catchPhrase'], function (err, result) {
 var pupil= new Students(result.name,result.gender,result.GPA,result.detentions,result.sleepingInClass,result.catchPhrase)
-makeBus();
+Busses.studentEntersBus(pupil);
 pupil.canHaveFun();
-
-
+Busses.busChatter();
+console.log(Busses.studentsOnTheBus);
+if (Busses.studentsOnTheBus.length<2) {
+makeStudent();
+}else{
+	console.log(studentsOnTheBus.busChatter);
+}
 });
 };
-
-
 makeStudent();
