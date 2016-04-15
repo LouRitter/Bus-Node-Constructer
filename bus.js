f7d5b7c312f40ec96d70da973e9c70931100eb1c
LouRitter <louritter1@gmail.com>
2016-04-15 13:45:36 -0400

    Kinda Works

--- bus.js
+++ bus.js
@@ -3,10 +3,5 @@ var bus= function(driverName, color, gas){
 	this.studentEntersBus= function(pupil){
 		this.studentsOnTheBus.push(pupil);
 	}
-	this.busChatter= function(){
-			if((this.studentsOnTheBus.GPA>2) && (this.studentsOnTheBus.detentions<10)){
-				console.log(studentsOnTheBus.catchPhrase);
-			};
-		};
-	}
+};
 module.exports= bus;
